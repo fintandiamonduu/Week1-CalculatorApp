bb4b9d462e2ae18d1b16cf86ee270d1894b9d941
Fintan Diamond <Diamond-F@ulster.ac.uk>
2024-09-24 15:03:51 +0100

    Implemented all of program code

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//start of  code
+////stores first of two number that will be added 
+//int numberone = 83;
+//store second number
+//int numbertwo = 123;
+//calling the result 
+//int result = numberone + numbertwo; 
+//Console.WriteLine("the combination of both numbers is " + result);
+
+//input first num
+Console.WriteLine("Type the first number followed by the eneter key");
+int firstnumber = Convert.ToInt32(Console.ReadLine());
+//input second num
+Console.WriteLine("Type the second number followed by the eneter key");
+int secondnumber = Convert.ToInt32(Console.ReadLine());
+//calculation 
+int result = firstnumber + secondnumber;
+//output answer into console 
+Console.WriteLine("adding {0} and {1} gets you {2}", firstnumber, secondnumber, result);
